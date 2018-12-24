45cdeb5675c809684753563df8c445f506148e22
Bruno Carballo <carballo.bruno@gmail.com>
2018-12-23 18:52:35 -0600

    add blog and jobs apps

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    summary = models.CharField(max_length = 200 )
+    image = models.ImageField(upload_to = 'images/')
+    content = models.TextField()
+
+
+
