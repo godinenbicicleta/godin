31bdce11cd3e16e1ff38eca9041be6b8f6558a16
Bruno Carballo <carballo.bruno@gmail.com>
2018-12-23 19:10:53 -0600

    add title of blog and add date field

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
-    summary = models.CharField(max_length = 200 )
+    title = models.CharField(max_length = 200 )
     image = models.ImageField(upload_to = 'images/')
-    content = models.TextField()
+    body = models.TextField()
+    pub_date = models.DateField()
