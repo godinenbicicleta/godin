ae71a92fafdae6daed302662a6ff46f1f9c652f3
Bruno Carballo <carballo.bruno@gmail.com>
2018-12-24 23:36:31 -0600

    django app ready to deploy

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,10 @@ class Blog(models.Model):
     image = models.ImageField(upload_to = 'images/')
     body = models.TextField()
     pub_date = models.DateField()
+    category = models.CharField(max_length = 40)
+
+    def __str__(self):
+        return f'{self.category}: {self.title}'
+
+    def summary(self):
+        return ''.join([self.body[:140],self.body[140:].split(' ')[0]])
